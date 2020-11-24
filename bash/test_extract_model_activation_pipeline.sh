6595a520a3b51a54099d70a024e7fc78d3f8fbf5
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-11-24 14:49:01 -0500

    bugfix

--- bash/test_extract_model_activation_pipeline.sh
+++ bash/test_extract_model_activation_pipeline.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #SBATCH --job-name=ext_mdl_act
-#SBATCH --array=0-1
+#SBATCH --array=0
 #SBATCH --time=96:00:00
 #SBATCH --ntasks=1
-#SBATCH --mem=120G
+#SBATCH --mem=180G
 #SBATCH --mail-type=ALL
 #SBATCH --exclude node017,node018
 #SBATCH --mail-user=ehoseini@mit.edu
 
 i=0
-for dataset in ud_sentences_token_filter_v3_sample ; do
-      for model in distilgpt2 gpt2 ; do
+for dataset in ud_sentences_token_filter_v3 ; do
+      for model in bert-large-uncased-whole-word-masking ; do
           model_list[$i]="$model"
           dataset_list[$i]="$dataset"
           i=$i+1
