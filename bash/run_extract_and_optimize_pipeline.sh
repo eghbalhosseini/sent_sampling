e4011cf78efa5279d7c390bfc6732e89bcdc9337
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-12-17 14:01:19 -0500

    bufgix

--- bash/run_extract_and_optimize_pipeline.sh
+++ bash/run_extract_and_optimize_pipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=ext_opt
-#SBATCH --array=0-9
+#SBATCH --array=0-4
 #SBATCH --time=12:00:00
 #SBATCH --mem=256G
 #SBATCH -c 16
@@ -35,7 +35,7 @@ for set in best_performing_pereira_1 \
  best_performing_pereira_4 \
  best_performing_pereira_5 ; do
   for idx in 0 ; do
-    for ave in True False ; do
+    for ave in False ; do
     for dataset in ud_sentencez_token_filter_v3 ; do
       extract_id="group=${set}-dataset=${dataset}-${extract_name[$idx]}-bench=${bench_type[$idx]}-ave=${ave}"
       extract_list[$i]="$extract_id"
