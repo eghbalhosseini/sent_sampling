d0b9fc6bceaa891586a45b5795416926168c27c9
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2021-01-17 19:54:36 -0500

    bugfix

--- test_optimization.py
+++ test_optimization.py
@@ -17,13 +17,15 @@ name='group=best_performing_pereira_5-dataset=ud_sentencez_token_filter_v3-activ
 
 import utils.optim_utils
 importlib.reload(utils.optim_utils)
-from utils.optim_utils import optim
+from utils.optim_utils import optim, optim_pool
 
 test=extract_pool[name]()
 
 test.load_dataset()
 test()
 
-optim_obj=optim(N_s=200,n_iter=100,n_init=1)
+optim_name='coordinate_ascent_eh-obj=D_s-n_iter=500-n_samples=10-n_init=1'
+optim_obj=optim_pool[optim_name]()
+
 optim_obj.load_extractor(test)
 S_opt_d, DS_opt_d=optim_obj()
