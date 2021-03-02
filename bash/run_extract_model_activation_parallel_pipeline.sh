e932667dc145cb29a461ce600418bd22a9090900
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2021-03-01 20:29:23 -0500

    parallel version for extraction

---
+++ bash/run_extract_model_activation_parallel_pipeline.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#SBATCH --job-name=ext_mdl_act
+#SBATCH --array=0-9
+#SBATCH --time=96:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem=180G
+#SBATCH --mail-type=ALL
+#SBATCH --exclude node017,node018
+#SBATCH --mail-user=ehoseini@mit.edu
+
+i=0
+for dataset in ud_sentencez_token_filter_v3 ; do
+  for group_ids in 0 1 2 3 4 5 6 7 8 9 ; do
+      for model in  distilgpt2 ; do
+          model_list[$i]="$model"
+          dataset_list[$i]="$dataset"
+          group_id_list[$i]=$group_id
+          i=$i+1
+      done
+done
+
+echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+echo "Running model ${model_list[$SLURM_ARRAY_TASK_ID]}"
+echo "Running dataset ${dataset_list[$SLURM_ARRAY_TASK_ID]}"
+echo "Running group ${group_id_list[$SLURM_ARRAY_TASK_ID]}"
+
+module add openmind/singularity
+export SINGULARITY_CACHEDIR=/om/user/`whoami`/st/
+RESULTCACHING_HOME=/om/user/`whoami`/.result_caching
+export RESULTCACHING_HOME
+XDG_CACHE_HOME=/om/user/`whoami`/st
+export XDG_CACHE_HOME
+singularity exec -B /om:/om /om/user/`whoami`/simg_images/neural_nlp_master.simg python /om/user/ehoseini/sent_sampling/extract_model_activations_parallel.py ${model_list[$SLURM_ARRAY_TASK_ID]} ${dataset_list[$SLURM_ARRAY_TASK_ID]} ${group_id_list[$SLURM_ARRAY_TASK_ID]}
+
+
+#transfo-xl-wt103 \
+#        t5-3b \
+#        xlnet-large-cased \
+#        bert-large-uncased-whole-word-masking \
+#        xlm-mlm-en-2048 \
+#        gpt2-xl \
+#        albert-xxlarge-v2 \
+#        ctrl
---
+++ extract_model_activations_parallel.py
@@ -0,0 +1,27 @@
+from utils import extract_pool
+from utils.extract_utils import model_extractor_parallel
+from utils.optim_utils import optim_pool
+import argparse
+from utils.data_utils import RESULTS_DIR, save_obj
+import os
+from utils.data_utils import SENTENCE_CONFIG
+parser = argparse.ArgumentParser(description='extract activations from a model')
+parser.add_argument('model_name', type=str,
+                    default='bert-base-uncased')
+parser.add_argument('dataset', type=str, default='ud_sentences_filter_v3_sample')
+parser.add_argument('group_id', type=int, default='group id for extraction, starts from 0')
+
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    model_id = args.model_name
+    dataset_id = args.dataset
+    group_id = int(args.group_id)
+    print(model_id+'\n')
+    print(dataset_id+'\n')
+    print(group_id + '\n')
+    # extract data
+    datafile=[x['file_loc'] for x in SENTENCE_CONFIG if x['name']==dataset_id][0]
+    extractor_obj = model_extractor_parallel(dataset=dataset_id, datafile=datafile, model_spec=model_id,average_sentence=True)
+    extractor_obj.load_dataset()
+    extractor_obj(group_id=group_id)
