cbb067b487597d749d8161ad6af23b32ba4c56fa
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2021-03-22 16:48:06 -0400

    adding new to config

--- utils/__init__.py
+++ utils/__init__.py
@@ -27,6 +27,15 @@ modl_name='gpt2-xl'
 layer_tuple=tuple([(modl_name,x) for x in model_layers[modl_name]])
 gpt2_xl_grp_config=dict(grp_id=f"{modl_name}_layers",grp_layer_tuple=tuple([layer_tuple[id] for id in range(0,len(layer_tuple))]),layer_by_name=True)
 
+modl_name='gpt2-medium'
+layer_tuple=tuple([(modl_name,x) for x in model_layers[modl_name]])
+gpt2_medium_config=dict(grp_id=f"{modl_name}_layers",grp_layer_tuple=tuple([layer_tuple[id] for id in range(0,len(layer_tuple))]),layer_by_name=True)
+
+modl_name='gpt2-large'
+layer_tuple=tuple([(modl_name,x) for x in model_layers[modl_name]])
+gpt2_large_config=dict(grp_id=f"{modl_name}_layers",grp_layer_tuple=tuple([layer_tuple[id] for id in range(0,len(layer_tuple))]),layer_by_name=True)
+
+
 modl_name='roberta-base'
 layer_tuple=tuple([(modl_name,x) for x in model_layers[modl_name]])
 roberta_base_grp_config=dict(grp_id= f"{modl_name}_layers",grp_layer_tuple=tuple([layer_tuple[id] for id in range(0,len(layer_tuple))]),layer_by_name=True)
@@ -54,6 +63,8 @@ ctrl_config=dict(grp_id= f"{modl_name}_layers",grp_layer_tuple=tuple([layer_tupl
 
 model_grps_config = [openaigpt_grp_config,
                      gpt2_grp_config,
+                     gpt2_medium_config,
+                     gpt2_large_config,
                      gpt2_xl_grp_config,
                      roberta_base_grp_config,
                      xlnet_large_cased_config,
