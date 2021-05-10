4e3e2738cbfd776d35e8a10760ed1a389eeea3f6
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2021-05-10 13:35:03 -0400

    running all averaging styles for gpt2 models

--- bash/run_extract_model_activation_parallel_pipeline.sh
+++ bash/run_extract_model_activation_parallel_pipeline.sh
@@ -12,11 +12,15 @@
 i=0
 for dataset in ud_sentencez_token_filter_v3 ; do
   for group_ids in 0 1 2 3 4 5 6 7 8 ; do
-      for model in t5-3b  ; do
+      for model in distilgpt2  ; do
+        for average in True False None ; do
           model_list[$i]="$model"
           dataset_list[$i]="$dataset"
           group_id_list[$i]=$group_ids
+          average_list[$i]=average
           i=$i+1
+
+          done
       done
       done
 done
@@ -25,11 +29,12 @@ echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
 echo "Running model ${model_list[$SLURM_ARRAY_TASK_ID]}"
 echo "Running dataset ${dataset_list[$SLURM_ARRAY_TASK_ID]}"
 echo "Running group ${group_id_list[$SLURM_ARRAY_TASK_ID]}"
+echo "Running average ${average_list[$SLURM_ARRAY_TASK_ID]}"
 
 export SINGULARITY_CACHEDIR=/om/user/`whoami`/st/
 RESULTCACHING_HOME=/om/user/`whoami`/.result_caching
 export RESULTCACHING_HOME
-singularity exec -B /om:/om /om/user/`whoami`/simg_images/neural_nlp_master.simg /usr/local/bin/python /om/user/ehoseini/sent_sampling/extract_model_activations_parallel.py ${model_list[$SLURM_ARRAY_TASK_ID]} ${dataset_list[$SLURM_ARRAY_TASK_ID]} ${group_id_list[$SLURM_ARRAY_TASK_ID]}
+singularity exec -B /om:/om /om/user/`whoami`/simg_images/neural_nlp_master.simg /usr/local/bin/python /om/user/ehoseini/sent_sampling/extract_model_activations_parallel.py ${model_list[$SLURM_ARRAY_TASK_ID]} ${dataset_list[$SLURM_ARRAY_TASK_ID]} ${group_id_list[$SLURM_ARRAY_TASK_ID]} ${average_list[$SLURM_ARRAY_TASK_ID]}
 
 
 #transfo-xl-wt103 \
--- extract_model_activations_parallel.py
+++ extract_model_activations_parallel.py
@@ -1,27 +1,24 @@
-from utils import extract_pool
 from utils.extract_utils import model_extractor_parallel
-from utils.optim_utils import optim_pool
 import argparse
-from utils.data_utils import RESULTS_DIR, save_obj
-import os
 from utils.data_utils import SENTENCE_CONFIG
 parser = argparse.ArgumentParser(description='extract activations from a model')
-parser.add_argument('model_name', type=str,
-                    default='bert-base-uncased')
+parser.add_argument('model_name', type=str,default='bert-base-uncased')
 parser.add_argument('dataset', type=str, default='ud_sentences_filter_v3_sample')
 parser.add_argument('group_id', type=int, default='group id for extraction, starts from 0')
-
+parser.add_argument('average_type', type=str, default='False')
 args = parser.parse_args()
 
 if __name__ == '__main__':
     model_id = args.model_name
     dataset_id = args.dataset
     group_id = int(args.group_id)
+    average_type=str(args.average_type)
     print(model_id+'\n')
     print(dataset_id+'\n')
     print(str(group_id) + '\n')
+    print(average_type + '\n')
     # extract data
     datafile=[x['file_loc'] for x in SENTENCE_CONFIG if x['name']==dataset_id][0]
-    extractor_obj = model_extractor_parallel(dataset=dataset_id, datafile=datafile, model_spec=model_id,average_sentence=False)
+    extractor_obj = model_extractor_parallel(dataset=dataset_id, datafile=datafile, model_spec=model_id,average_sentence=average_type)
     extractor_obj.load_dataset()
     extractor_obj(group_id=group_id)
