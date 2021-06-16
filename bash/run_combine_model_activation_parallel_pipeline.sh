d3d0a212d8008b7aadb5d5d7167e4427fcba58d3
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2021-06-16 07:52:00 -0400

    combine gpt2-xl

--- bash/run_combine_model_activation_parallel_pipeline.sh
+++ bash/run_combine_model_activation_parallel_pipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=CM_PA
-#SBATCH --array=0-2
+#SBATCH --array=0-71
 #SBATCH --time=168:00:00
 #SBATCH --ntasks=1
 #SBATCH --mem=80G
@@ -10,8 +10,12 @@
 #SBATCH --mail-user=ehoseini@mit.edu
 
 i=0
-for dataset in ud_sentencez_token_filter_v3 coca_spok_filter_punct_10K_sample_1 ; do
-      for model in gpt2-xl-untrained ; do
+for dataset in coca_spok_filter_punct_10K_sample_1 \
+                coca_spok_filter_punct_10K_sample_2 \
+               # coca_spok_filter_punct_10K_sample_3 \
+                coca_spok_filter_punct_10K_sample_4 \
+                coca_spok_filter_punct_10K_sample_5 ; do
+      for model in ctrl gpt2-xl lm_1b gpt2 openaigpt bert-large-uncased-whole-word-masking  ; do
               for average_mode in False True None ; do
                   model_list[$i]="$model"
                   dataset_list[$i]="$dataset"
