39fe0f829b1dd608955aea6a9eb22d1ca2c76838
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2021-12-31 11:51:19 -0500

    find missing files

--- bash/run_extract_model_act_par_on_missing_cocal_all.sh
+++ bash/run_extract_model_act_par_on_missing_cocal_all.sh
@@ -31,8 +31,6 @@ for dataset in  coca_preprocessed_all_clean_100K_sample_1 ; do
               echo "${lines} vs ${layer_arr[$idx_model]}  - ${dataset}_${model}_group_${group_ids} dosent exists, adding it \n"
               LINE_COUNT=$(expr ${LINE_COUNT} + 1)
               printf "%d,%s,%s,%d\n" "$LINE_COUNT" "$model" "$dataset" "$group_ids" >> $GRAND_PIPE_FILE
-          else
-              pass
           fi
       done
       done
