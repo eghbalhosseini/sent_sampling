be3a1b2f41d80a76cf86cc6b0e47c3deaaa817dc
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2021-12-31 11:48:15 -0500

    find missing files

--- bash/run_extract_model_act_par_on_missing_cocal_all.sh
+++ bash/run_extract_model_act_par_on_missing_cocal_all.sh
@@ -23,12 +23,12 @@ for dataset in  coca_preprocessed_all_clean_100K_sample_1 ; do
           dataset_list[$i]="$dataset"
           group_id_list[$i]=$group_ids
           i=$i+1
-          look_up_pattern="${dataset}_${model}_*_group_${group_ids}*"
+          look_up_pattern="${dataset}_${model}_*_group_${group_ids}*.pkl"
           folder_to_look=${DATA_DIR}/${model}
           printf "finding pattern ${look_up_pattern} \n"
           #lines=$(find $folder_to_look -name "${dataset}_${model}_*_group_${group_ids}*.pkl" | wc -l)
           lines=$(find $folder_to_look -name $look_up_pattern | wc -l)
-          if [ "$lines" -eq "${layer_arr[$idx_model]}" ]; then
+          if [ "$lines" = "${layer_arr[$idx_model]}" ]; then
               echo "${dataset}_${model}_group_${group_ids} dosent exists, adding it \n"
               LINE_COUNT=$(expr ${LINE_COUNT} + 1)
               printf "%d,%s,%s,%d\n" "$LINE_COUNT" "$model" "$dataset" "$group_ids" >> $GRAND_PIPE_FILE
