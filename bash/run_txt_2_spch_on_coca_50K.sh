d1c98caf895bced9090950f68398ead8bc0407a7
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-03-23 19:30:16 -0400

    bugfix

--- bash/run_txt_2_spch_on_coca_50K.sh
+++ bash/run_txt_2_spch_on_coca_50K.sh
@@ -3,10 +3,10 @@ DATA_DIR=/om/user/ehoseini/MyData/sent_sampling/
 i=0
 i=$(expr ${i} + 0)
 LINE_COUNT=0
-GRAND_PIPE_FILE="${DATA_DIR}/Grand_coca_txt_2_spch_pipe_list.csv"
+GRAND_PIPE_FILE="${DATA_DIR}/Grand_coca_txt_2_spch_pipe_list.txt"
 rm -f $GRAND_PIPE_FILE
 touch $GRAND_PIPE_FILE
-printf "%s,%s,%s,%s\n" "row" "id" "sentence" "file"  >> $GRAND_PIPE_FILE
+printf "%s\t%s\t%s\t%s\n" "row" "id" "sentence" "file"  >> $GRAND_PIPE_FILE
 while read string; do
       framename=$(printf '%05d' $i)
       possible_file="/om/user/ehoseini/MyData/sent_sampling/coca_spok_filter_punct_50K/wav//coca_spok_filter_punct_50K_${framename}.wav"
@@ -15,7 +15,7 @@ while read string; do
         true
       else
         LINE_COUNT=$(expr ${LINE_COUNT} + 1)
-        printf "%d,%s,%s,%s\n" "$LINE_COUNT" "$framename" "$string" "$possible_file"  >> $GRAND_PIPE_FILE
+        printf "%d\t%s\t%s\t%s\n" "$LINE_COUNT" "$framename" "$string" "$possible_file"  >> $GRAND_PIPE_FILE
   fi
     i=$(expr ${i} + 1)
 done < /om/user/ehoseini/MyData/sent_sampling/coca_spok_filter_punct_50K/text/coca_spok_filter_punct_50K_all.txt
