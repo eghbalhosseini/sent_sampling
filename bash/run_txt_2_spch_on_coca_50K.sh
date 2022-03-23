e4e3afdd5b0f37142f4f07168f238302809e35eb
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-03-23 19:04:22 -0400

    added functinality for removing previously proceessed sentences

---
+++ bash/run_txt_2_spch_on_coca_50K.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+DATA_DIR=/om2/user/ehoseini/MyData/sent_sampling/
+LINE_COUNT=0
+GRAND_PIPE_FILE="${DATA_DIR}/Grand_coca_txt_2_spch_pipe_list.csv"
+rm -f $GRAND_PIPE_FILE
+touch $GRAND_PIPE_FILE
+printf "%s,%s,%s,%s\n" "row" "id" "sentence" "file"  >> $GRAND_PIPE_FILE
+while IFS=, read -r line_count string; do
+      possible_file="/om/user/ehoseini/MyData/sent_sampling/coca_spok_filter_punct_50K/wav//coca_spok_filter_punct_50K_${line_count}.wav"
+      if [ -f "$possible_file" ]
+      then
+        true
+      else
+        LINE_COUNT=$(expr ${LINE_COUNT} + 1)
+        printf "%d,%s,%s,%s\n" "$LINE_COUNT" "$line_count" "$string" "$possible_file"  >> $GRAND_PIPE_FILE
+  fi
+done < /om/user/ehoseini/MyData/sent_sampling/coca_spok_filter_punct_50K/text/coca_spok_filter_punct_50K_all.txt
+
+echo $LINE_COUNT
+run_val=0
+if [ "$LINE_COUNT" -gt "$run_val" ]; then
+  echo "running  ${LINE_COUNT} "
+   #nohup /cm/shared/admin/bin/submit-many-jobs $LINE_COUNT 20 25 5 extract_model_act_for_group.sh $GRAND_PIPE_FILE
+   #nohup /cm/shared/admin/bin/submit-many-jobs $LINE_COUNT  250 300 50 extract_model_act_for_group.sh $GRAND_PIPE_FILE
+  else
+    echo $LINE_COUNT
+fi
---
+++ combine_coca_dataset_for_gpt2_xl.py
@@ -0,0 +1,65 @@
+from utils import extract_pool
+from utils.extract_utils import model_extractor
+from utils.data_utils import RESULTS_DIR, save_obj,load_obj,SAVE_DIR
+from utils.data_utils import SENTENCE_CONFIG
+import os
+import numpy as np
+from tqdm import tqdm
+from pathlib import Path
+
+if __name__ == '__main__':
+    #dataset_id='coca_spok_filter_punct_10K_sample_1'
+
+    #extractor_id=f'group=gpt2-xl_layers-dataset={dataset_id}-activation-bench=None-ave=None'
+    #extractor_obj = extract_pool[extractor_id]()
+    #extractor_obj.load_dataset()
+    #text_lines=[x['text'] for x in extractor_obj.data_]
+    #text_lines[1]
+    #extractor_obj(overwrite=False)
+    #extractor_obj.model_group_act[0]['activations'][0]
+
+
+    for l in tqdm(range(49)):
+        t_all = []
+        for sample_1 in range(5):
+            dataset_id = f'coca_spok_filter_punct_10K_sample_{sample_1+1}'
+            extractor_id = f'group=gpt2-xl_layers-dataset={dataset_id}-activation-bench=None-ave=None'
+            extractor_obj = extract_pool[extractor_id]()
+            model_actvation_name = f"{extractor_obj.dataset}_{extractor_obj.model_spec[l]}_layer_{extractor_obj.layer_spec[l]}_{extractor_obj.extract_name}_ave_{extractor_obj.average_sentence}.pkl"
+            t=load_obj(os.path.join(SAVE_DIR,model_actvation_name))
+            t_all.append(t)
+
+        flat_list = [item for sublist in t_all for item in sublist]
+        new_dataset_id = f'coca_spok_filter_punct_50K'
+        new_model_actvation_name = f"{new_dataset_id}_{extractor_obj.model_spec[l]}_layer_{extractor_obj.layer_spec[l]}_{extractor_obj.extract_name}_ave_{extractor_obj.average_sentence}.pkl"
+        save_obj(flat_list,os.path.join(SAVE_DIR, new_model_actvation_name))
+    # create a folder that contains text and wav files
+    txt_path=Path(SAVE_DIR,new_dataset_id,'text')
+    txt_path.mkdir(parents=True, exist_ok=True)
+    sentence_text = [x[1] for x in flat_list]
+    txt_file = f'{txt_path.__str__()}/{new_dataset_id}_all.txt'
+    with open(txt_file,'w') as f:
+        for line_id,line in tqdm(enumerate(sentence_text)):
+            id_z = str(line_id).zfill(5)
+            f.write(f'{id_z},{line}')
+            f.write('\n')
+    f.close()
+    for id, txt in tqdm(enumerate(sentence_text)):
+        id_z=str(id).zfill(5)
+        txt_file=f'{txt_path.__str__()}/{new_dataset_id}_{id_z}.txt'
+        text_file = open(txt_file, "w")
+        n = text_file.write(txt)
+        text_file.close()
+
+
+
+    # remove duplicates from the set
+
+    unique_sentences = list(set(sentence_text))
+    unique_sentences=np.unique(sentence_text)
+    duplicate_indices=[]
+    for id, x in tqdm(enumerate(unique_sentences)):
+        duplicate_indices.append(sentence_text.index(x))
+    np.unique(duplicate_indices).shape
+    sentence_data_filter_no_dup = [sentence_data_filter_len[x] for x in duplicate_indices]
+    print(f'Filtered out {len(sentence_data_filter_len) - len(sentence_data_filter_no_dup)} duplicate sentences')
