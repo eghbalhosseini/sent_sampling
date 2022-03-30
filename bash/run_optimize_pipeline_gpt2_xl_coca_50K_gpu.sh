2ed3856bfd392dc2ff18638c8a1b1d7d1fd9a233
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-03-29 20:26:39 -0400

    bugfix

--- bash/run_optimize_pipeline_gpt2_xl_coca_50K_gpu.sh
+++ bash/run_optimize_pipeline_gpt2_xl_coca_50K_gpu.sh
@@ -25,4 +25,4 @@ conda activate neural_nlp_cuda
 echo $(which python)
 cd /om/user/ehoseini/sent_sampling/
 
-python extract_and_optimize_on_gpu_gpt2_xl_coca_50K.py
+python extract_and_optimize_on_gpu_gpt2_xl_coca_50K.py "group=gpt2-xl_layers-dataset=coca_spok_filter_punct_50K_sylb-activation-bench=None-ave=False" "coordinate_ascent_eh-obj=D_s-n_iter=1000-n_samples=250-n_init=1-run_gpu=True"
