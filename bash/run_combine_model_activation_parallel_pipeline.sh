ec83ca36710fd875dbf3ed4fe8f99324003cef91
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2023-01-10 10:06:06 -0500

    adding combining operation

--- bash/run_combine_model_activation_parallel_pipeline.sh
+++ bash/run_combine_model_activation_parallel_pipeline.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
 #SBATCH --job-name=CM_PA
-#SBATCH --array=0-17
+#SBATCH --array=0-42
 #SBATCH --time=168:00:00
 #SBATCH --ntasks=1
-#SBATCH --mem=256G
+#SBATCH --mem=40G
 #SBATCH --mail-type=ALL
 #SBATCH --exclude node017,node018
 #SBATCH --mail-user=ehoseini@mit.edu
 
 i=0
-for dataset in coca_spok_filter_punct_10K_sample_ev_editsOct16 ; do
-      for model in bert-large-uncased-whole-word-masking \
-       gpt2-xl \
-       ctrl \
-       gpt2 \
-       openaigpt \
-       lm_1b ; do
+for dataset in ud_sentencez_token_filter_v3 ; do
+      for stim_type in textPeriod textNoPeriod ; do
+      for model in roberta-base xlnet-large-cased bert-large-uncased-whole-word-masking \
+          xlm-mlm-en-2048 gpt2-xl albert-xxlarge-v2 ctrl ; do
               for average_mode in False True None ; do
                   model_list[$i]="$model"
                   dataset_list[$i]="$dataset"
                   average_list[$i]="$average_mode"
-                  dataset_list[$i]="$dataset"
+                  stim_type_list[$i]="$stim_type"
                   i=$i+1
               done
+          done
       done
 done
 
@@ -31,35 +29,17 @@ echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
 echo "Running model ${model_list[$SLURM_ARRAY_TASK_ID]}"
 echo "Running dataset ${dataset_list[$SLURM_ARRAY_TASK_ID]}"
 echo "Running average_mode ${average_list[$SLURM_ARRAY_TASK_ID]}"
+echo "Running stim type ${stim_type_list[$SLURM_ARRAY_TASK_ID]}"
 
+module add openmind/singularity
 export SINGULARITY_CACHEDIR=/om/user/`whoami`/st/
-RESULTCACHING_HOME=/om/user/`whoami`/.result_caching
+RESULTCACHING_HOME=/om5/group/evlab/u/ehoseini/.result_caching
 export RESULTCACHING_HOME
+XDG_CACHE_HOME=/om/user/`whoami`/st
+export XDG_CACHE_HOME
 
+. /om/user/ehoseini/.bash_profile
+. /om/user/ehoseini/.bashrc
+conda activate neural_nlp_2022
 
-
-singularity exec -B /om:/om,/om2:/om2 /om/user/`whoami`/simg_images/neural_nlp_master.simg /usr/local/bin/python /om/user/ehoseini/sent_sampling/combine_model_activations_parallel.py ${model_list[$SLURM_ARRAY_TASK_ID]} ${dataset_list[$SLURM_ARRAY_TASK_ID]} ${average_list[$SLURM_ARRAY_TASK_ID]}
-
-#for dataset in coca_spok_filter_punct_10K_sample_1 \
-# coca_spok_filter_punct_10K_sample_2 \
-# coca_spok_filter_punct_10K_sample_3 \
-# coca_spok_filter_punct_10K_sample_4 \
-# coca_spok_filter_punct_10K_sample_5 ; do
-#      for model in gpt2-xl gpt2-large gpt2-medium gpt2 distilgpt2 openaigpt \
-#          albert-xxlarge-v2 albert-xlarge-v2 \
-#          t5-11b t5-3b t5-large \
-#          xlnet-large-cased \
-#          ctrl \
-#          bert-large-uncased-whole-word-masking distilbert-base-uncased \
-#          xlm-mlm-en-2048 \
-#          transfo-xl-wt103 \
-#          roberta-base; do
-#              for average_mode in True False None ; do
-#                  model_list[$i]="$model"
-#                  dataset_list[$i]="$dataset"
-#                  average_list[$i]="$average_mode"
-#                  dataset_list[$i]="$dataset"
-#                  i=$i+1
-#              done
-#      done
-#done
+/om/user/ehoseini/miniconda3/envs/neural_nlp_2022/bin/python /om/user/ehoseini/sent_sampling/combine_model_activations_parallel.py ${model_list[$SLURM_ARRAY_TASK_ID]} ${dataset_list[$SLURM_ARRAY_TASK_ID]} ${stim_type_list[$SLURM_ARRAY_TASK_ID]} ${average_list[$SLURM_ARRAY_TASK_ID]}
