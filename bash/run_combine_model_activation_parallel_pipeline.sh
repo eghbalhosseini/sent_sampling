711490d69334760800515bfaf6d0ddcc33c5c18e
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2023-01-22 16:52:43 -0500

    combining text no period files

--- bash/run_combine_model_activation_parallel_pipeline.sh
+++ bash/run_combine_model_activation_parallel_pipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=CM_PA
-#SBATCH --array=0-42
+#SBATCH --array=0-20
 #SBATCH --time=168:00:00
 #SBATCH --ntasks=1
 #SBATCH --mem=40G
@@ -11,7 +11,7 @@
 
 i=0
 for dataset in ud_sentencez_token_filter_v3 ; do
-      for stim_type in textNoPeriod wordFORM ; do
+      for stim_type in textNoPeriod ; do
       for model in roberta-base xlnet-large-cased bert-large-uncased-whole-word-masking \
           xlm-mlm-en-2048 gpt2-xl albert-xxlarge-v2 ctrl ; do
               for average_mode in False True None ; do
