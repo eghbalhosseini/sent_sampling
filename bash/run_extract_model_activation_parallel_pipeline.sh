05b2c09496c9f93d36a7294fce480b3c276a9c4b
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2023-03-20 10:28:42 -0400

    created option for shorthand saving

--- bash/run_extract_model_activation_parallel_pipeline.sh
+++ bash/run_extract_model_activation_parallel_pipeline.sh
@@ -4,7 +4,7 @@
 #SBATCH --array=0-114
 #SBATCH --time=3:00:00
 #SBATCH --ntasks=1
-#SBATCH --mem=10G
+#SBATCH --mem=20G
 #SBATCH --mail-type=ALL
 #SBATCH --exclude node017,node018
 #SBATCH --mail-user=ehoseini@mit.edu
