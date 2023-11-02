6e9374967077a68019c432d733d03d83b198020c
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2023-11-02 09:00:29 -0400

    bugfix

--- bash/run_extract_model_act_for_group.sh
+++ bash/run_extract_model_act_for_group.sh
@@ -36,14 +36,14 @@ for dataset in  coca_preprocessed_all_clean_no_dup_100K_sample_1 ; do
           #echo $look_up_pattern
           folder_to_look=${DATA_DIR}/${model}
           #lines=$(find $folder_to_look -name "${dataset}_${model}_*_group_${group_ids}*.pkl" | wc -l)
-          lines=$(find $folder_to_look -name $look_up_pattern | wc -l)
-          echo $lines
-          if [ "$lines" != "${layer_arr[$idx_model]}" ]; then
-              echo "${lines} vs ${layer_arr[$idx_model]}  - ${dataset}_${stim_type}_${model}_group_${group_ids} dosent exists, adding it \n"
-              LINE_COUNT=$(expr ${LINE_COUNT} + 1)
-              printf "%d,%s,%s,%s,%s,%d\n" "$LINE_COUNT" "$model" "$dataset" "$stim_type" "$splits" "$group_ids" >> $GRAND_PIPE_FILE
-
-          fi
+#          lines=$(find $folder_to_look -name $look_up_pattern | wc -l)
+#          echo $lines
+#          if [ "$lines" != "${layer_arr[$idx_model]}" ]; then
+#              echo "${lines} vs ${layer_arr[$idx_model]}  - ${dataset}_${stim_type}_${model}_group_${group_ids} dosent exists, adding it \n"
+#              LINE_COUNT=$(expr ${LINE_COUNT} + 1)
+#              printf "%d,%s,%s,%s,%s,%d\n" "$LINE_COUNT" "$model" "$dataset" "$stim_type" "$splits" "$group_ids" >> $GRAND_PIPE_FILE
+#
+#          fi
         done
       done
     done
