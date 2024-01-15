ed22b852f57754c79d05f902cc1acd6edb96de8c
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2024-01-15 09:58:27 -0500

    adding code to get curvature for llama_2 models

--- bash/straightening/run_straightening_pipeline_in_huggingface_for_LLAMA_2.sh
+++ bash/straightening/run_straightening_pipeline_in_huggingface_for_LLAMA_2.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 #SBATCH --job-name=LLAMA
-#SBATCH --array=0-1
+#SBATCH --array=0
 #SBATCH --time=12:00:00
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:a100:4
 #SBATCH --constraint=high-capacity
-#SBATCH --mem=180G
+#SBATCH --mem=256G
 #SBATCH --mail-type=ALL
 #SBATCH --exclude node017,node018
 #SBATCH --mail-user=ehoseini@mit.edu
 
 i=0
-for model in  70B 70Bf; do
+for model in  70B ; do
             model_list[$i]="$model"
             i=$i+1
 done
