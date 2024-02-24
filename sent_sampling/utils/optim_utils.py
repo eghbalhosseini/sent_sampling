a3fdec2b582d04b20489415eb64b52424a702bbe
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2024-02-24 08:40:44 -0500

    bugfix

--- sent_sampling/utils/optim_utils.py
+++ sent_sampling/utils/optim_utils.py
@@ -469,7 +469,7 @@ objective_function=[dict(name='D_s',fun=Distance),dict(name='D_s_var',fun=Distan
                     dict(name='2-D_s',fun=minus_Distance)]
 
 n_iters=[2,50,100,500]
-N_s=[10,25,50,75,100,125,150,175,200,225,250,275,300]
+N_s=[10,25,50,75,80,100,125,150,175,200,225,250,275,300]
 n_inits=[1,2]
 run_gpu=[True,False]
 low_dim=[True,False]
