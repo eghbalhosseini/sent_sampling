f96f027b0dfe96cbc7f310905ff8a88bbf824465
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2024-03-23 09:21:35 -0400

    bugfix

--- bash/run_combine_model_activation_parallel_per_layer_pipeline.sh
+++ bash/run_combine_model_activation_parallel_per_layer_pipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=CM_PA
-#SBATCH --array=0
+#SBATCH --array=0-7
 #SBATCH --time=24:00:00
 #SBATCH --mem=128G
 #SBATCH --mail-type=ALL
@@ -9,27 +9,28 @@
 #SBATCH --mail-user=ehoseini@mit.edu
 
 i=0
-split=200
-#neural_ctrl_stim
+models="roberta-base xlnet-large-cased bert-large-uncased-whole-word-masking xlm-mlm-en-2048 albert-xxlarge-v2 ctrl"
+layers="2 24 12 12 5 47" #
+model_arr=($models)
+layer_arr=($layers)
+split=100
+len=${#layer_arr[@]}
 # coca_preprocessed_all_clean_100K_sample_1
 #roberta-base xlnet-large-cased bert-large-uncased-whole-word-masking \
 #        xlm-mlm-en-2048 gpt2-xl albert-xxlarge-v2 ctrl
-for dataset in coca_preprocessed_all_clean_100K_sample_1_2_ds_min_est_n_10K  ; do
-    for stim_type in textNoPeriod ; do
-      for model in  gpt2-xl ; do
-         # sequence of 0 to 48 for gpt2-xl
-        for layer_id in 29 ; do
-              for average_mode in False ; do
-                  model_list[$i]="$model"
-                  dataset_list[$i]="$dataset"
-                  average_list[$i]="$average_mode"
-                  layer_id_list[$i]="$layer_id"
-                  stim_type_list[$i]="$stim_type"
-                  i=$i+1
-              done
-        done
+for dataset in coca_preprocessed_all_clean_no_dup_100K_sample_1  ; do
+  for stim_type in textNoPeriod ; do
+    for (( idx_model=0; idx_model<$len; idx_model++ )) ; do
+      for average_mode in False ; do
+        model_list[$i]="${model_arr[$idx_model]}"
+        layer_list[$i]="${layer_arr[$idx_model]}"
+        dataset_list[$i]="$dataset"
+        average_list[$i]="$average_mode"
+        stim_type_list[$i]="$stim_type"
+        i=$i+1
       done
     done
+  done
 done
 
 echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
@@ -39,7 +40,6 @@ echo "Running average_mode ${average_list[$SLURM_ARRAY_TASK_ID]}"
 echo "Running stim type ${stim_type_list[$SLURM_ARRAY_TASK_ID]}"
 
 module add openmind/singularity
-export SINGULARITY_CACHEDIR=/om/user/`whoami`/st/
 RESULTCACHING_HOME=/om5/group/evlab/u/ehoseini/.result_caching
 export RESULTCACHING_HOME
 XDG_CACHE_HOME=/om/user/`whoami`/st
@@ -49,4 +49,4 @@ export XDG_CACHE_HOME
 . /om/user/ehoseini/.bashrc
 conda activate neural_nlp_2022
 
-/om/user/ehoseini/miniconda3/envs/neural_nlp_2022/bin/python /om/user/ehoseini/sent_sampling/combine_model_activations_parallel_per_layer.py ${model_list[$SLURM_ARRAY_TASK_ID]} ${dataset_list[$SLURM_ARRAY_TASK_ID]} ${stim_type_list[$SLURM_ARRAY_TASK_ID]} ${average_list[$SLURM_ARRAY_TASK_ID]} ${split} ${layer_id_list[$SLURM_ARRAY_TASK_ID]}
+/om/user/ehoseini/miniconda3/envs/neural_nlp_2022/bin/python /om2/user/ehoseini/sent_sampling/sampling/combine_model_activations_parallel_per_layer.py ${model_list[$SLURM_ARRAY_TASK_ID]} ${dataset_list[$SLURM_ARRAY_TASK_ID]} ${stim_type_list[$SLURM_ARRAY_TASK_ID]} ${average_list[$SLURM_ARRAY_TASK_ID]} ${split} ${layer_list[$SLURM_ARRAY_TASK_ID]}
