567c250644a3d6f620b6a9da98f9ffa4b4634cb3
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2024-03-24 11:20:32 -0400

    bugfix

--- bash/run_extract_model_act_for_group.sh
+++ bash/run_extract_model_act_for_group.sh
@@ -5,7 +5,7 @@ LINE_COUNT=0
 GRAND_PIPE_FILE="${DATA_DIR}/Grand_extraction_pipe_list.csv"
 rm -f $GRAND_PIPE_FILE
 touch $GRAND_PIPE_FILE
-models="roberta-base xlnet-large-cased bert-large-uncased-whole-word-masking xlm-mlm-en-2048 albert-xxlarge-v2 ctrl"
+models="ctrl gpt2-xl"
 layers="13 25 25 13 13 49" # adding the embedding layer so its layer plus 1
 #models="gpt2-xl"
 #layers="49" # adding the embedding layer so its layer plus 1
