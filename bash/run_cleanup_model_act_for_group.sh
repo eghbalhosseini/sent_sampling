c4b8f646120b44e07d29c221fe716cfb6beb0e46
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2024-06-09 15:41:52 -0400

    bugfix

--- bash/run_cleanup_model_act_for_group.sh
+++ bash/run_cleanup_model_act_for_group.sh
@@ -48,6 +48,21 @@ for dataset in  ud_sentences_U01_SET1_paraphrase ; do
             rm $file
           done
 
+          # delete the crunched files
+          #ud_sentences_U01_SET1_paraphrase_albert-xxlarge-v2_layer_2_activation_ave_False.pkl
+
+
+        done
+        look_up_pattern="${dataset}${stim_type}_${model}_layer_*_activation_*.pkl"
+        folder_to_look=${DATA_DIR}/
+        echo $look_up_pattern
+
+        lines=$(find $folder_to_look -name $look_up_pattern | wc -l)
+        echo $lines
+        # delete the files from find and print which files are deleted
+        for file in $(find $folder_to_look -name $look_up_pattern); do
+          echo "deleting $file"
+          rm $file
         done
       done
     done
