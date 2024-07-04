6d3732e90948848236586aed055b05bed79c5614
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2024-07-04 14:17:00 +0100

    detaching opt_exp_design repo

--- bash/om_run_script_on_gpu.sh
+++ bash/om_run_script_on_gpu.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=opt
-#SBATCH --time=48:00:00
+#SBATCH --time=36:00:00
 #SBATCH --mem=180G
 #SBATCH --gres=gpu:a100:2
 #SBATCH --constraint=high-capacity
 #SBATCH --mail-type=ALL
 #SBATCH --exclude node017,node018
 #SBATCH --mail-user=ehoseini@mit.edu
-#SBATCH -p evlab
 
 PYTHON_SCR=$1
 echo "running :${PYTHON_SCR}"
