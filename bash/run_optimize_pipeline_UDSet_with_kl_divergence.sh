341fa13314642151499c8c661e303c6f8c4de693
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2024-07-31 19:18:51 -0400

    bugfix

---
+++ bash/run_optimize_pipeline_UDSet_with_kl_divergence.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#SBATCH --job-name=opt_kl
+#SBATCH --array=0-15
+#SBATCH --time=48:00:00
+#SBATCH --mem=64G
+#SBATCH --gres=gpu:1
+#SBATCH --constraint=high-capacity
+#SBATCH --mail-type=ALL
+#SBATCH --exclude node017,node018
+#SBATCH --mail-user=ehoseini@mit.edu
+
+i=0
+for multply in 5 10 ; do
+  for theshold in 0.01 0.05 0.1 0.2 ; do
+      for ds in D_s_kl_div 2-D_s_kl_div ; do
+        optim_id="coordinate_ascent_eh-obj=${ds}-n_iter=50-n_samples=225-n_init=1-low_dim=False-pca_var=0.9-pca_type=pytorch-run_gpu=True"
+        optim_list[$i]="$optim_id"
+        multiply_list[$i]="$multply"
+        theshold_list[$i]="$theshold"
+        i=$i+1
+      done
+  done
+done
+
+
+
+
+echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+echo "Running optimiation: ${optim_list[$SLURM_ARRAY_TASK_ID]}"
+echo "Running multiply: ${multiply_list[$SLURM_ARRAY_TASK_ID]}"
+echo "Running theshold: ${theshold_list[$SLURM_ARRAY_TASK_ID]}"
+
+
+RESULTCACHING_HOME=/om5/group/evlab/u/ehoseini/.result_caching
+export RESULTCACHING_HOME
+
+
+. /om/user/ehoseini/.bash_profile
+. /om/user/ehoseini/.bashrc
+conda activate neural_nlp_2022
+
+/om/user/ehoseini/miniconda3/envs/neural_nlp_2022/bin/python /om2/user/ehoseini/sent_sampling/sampling/optimize_ANNSet1_on_UDset_kl_divergence.py ${optim_list[$SLURM_ARRAY_TASK_ID]} ${multiply_list[$SLURM_ARRAY_TASK_ID]} ${theshold_list[$SLURM_ARRAY_TASK_ID]}
--- sampling/optimize_ANNSet1_on_UDset_kl_divergence.py
+++ sampling/optimize_ANNSet1_on_UDset_kl_divergence.py
@@ -15,15 +15,26 @@ import os
 import torch
 from tqdm import tqdm
 import numpy as np
+import argparse
+parser = argparse.ArgumentParser(description='extract activations and optimize')
+parser.add_argument('optimizer_id', type=str, default='coordinate_ascent-obj=D_s-n_iter=100-n_samples=100-n_init=1')
+parser.add_argument('multiplier', type=float, default=1)
+parser.add_argument('threshold', type=float, default=0.05)
+
+args = parser.parse_args()
 
 if __name__ == '__main__':
+    optimizer_id = args.optimizer_id
+    kl_multiplier = float(args.multiplier)
+    kl_threshold = float(args.threshold)
     extract_id='group=best_performing_pereira_1-dataset=ud_sentencez_token_filter_v3_minus_ev_sentences_textNoPeriod-activation-bench=None-ave=False'
+
     extractor_obj = extract_pool[extract_id]()
     extractor_obj.load_dataset()
     extractor_obj()
     # extract ev sentences
     # find location of ev sentences in sentences
-    optimizer_id = f"coordinate_ascent_eh-obj=D_s_kl_div-n_iter=50-n_samples=225-n_init=1-low_dim=False-pca_var=0.9-pca_type=pytorch-run_gpu=True"
+    #optimizer_id = f"coordinate_ascent_eh-obj=D_s_kl_div-n_iter=50-n_samples=225-n_init=1-low_dim=False-pca_var=0.9-pca_type=pytorch-run_gpu=True"
     optimizer_obj = optim_pool[optimizer_id]()
     optimizer_obj.load_extractor(extractor_obj)
     low_resolution= False
@@ -68,7 +79,7 @@ if __name__ == '__main__':
 
 
     kl_div_rnd = []
-    for kk in tqdm(range(100)):
+    for kk in tqdm(range(1000)):
         S = np.random.choice(optimizer_obj.N_S, optimizer_obj.N_s, replace=False)
         # compute objective function for the random sample
         samples = torch.tensor(S, dtype=torch.long, device=optimizer_obj.device)
@@ -86,34 +97,41 @@ if __name__ == '__main__':
     kl_div_rnd_max=kl_div_rnd.max(dim=0)[0]
 
 
-
     optimizer_obj.XY_corr_hist_mean = XY_corr_hist_mean
     optimizer_obj.bins = bins
-    optimizer_obj.kl_div_rnd_max=10*kl_div_rnd_max
+    optimizer_obj.kl_div_rnd_max=kl_multiplier*kl_div_rnd_max
     optimizer_obj.epsilon=1e-10
-    optimizer_obj.kl_div_threshold=.05
-    optimizer_obj.kl_div_muliplier=1
+    optimizer_obj.kl_div_threshold=kl_threshold
+    optimizer_obj.kl_div_muliplier=kl_multiplier
     optimizer_obj.corr_min_max=list(zip(min_vals,max_vals))
 
-    optimizer_obj.gpu_object_function_ds_kl_div(S, debug=True)
-
-
-    S_opt_d, DS_opt_d = optimizer_obj()
-
-    optim_results = dict(extractor_name=extract_id,
-                         model_spec=extractor_obj.model_spec,
-                         layer_spec=extractor_obj.layer_spec,
-                         data_type=extractor_obj.extract_type,
-                         benchmark=extractor_obj.extract_benchmark,
-                         average=extractor_obj.average_sentence,
-                         optimizatin_name=optimizer_id,
-                         optimized_S=S_opt_d,
-                         optimized_d=DS_opt_d)
-
-    [ext_id,opt_id]=make_shorthand(extract_id,optimizer_id)
-
+    [ext_id, opt_id] = make_shorthand(extract_id, optimizer_id)
     optim_file = os.path.join(RESULTS_DIR,
-                              f"results_{ext_id}_{opt_id}_kl_thr_{optimizer_obj.kl_div_threshold}_mult_{optimizer_obj.kl_div_muliplier}_bins_{optimizer_obj.bins}_norm.pkl")
-    # check of path is too long
-    save_obj(optim_results, optim_file)
+                              f"res_{ext_id}_{opt_id}_kl_thr_{optimizer_obj.kl_div_threshold}_mult_{optimizer_obj.kl_div_muliplier}_bins_{optimizer_obj.bins}_norm.pkl")
+
+    if os.path.exists(optim_file):
+        print(f"file {optim_file} exists, skipping optimization")
+        optim_results = load_obj(optim_file)
+        S_opt_d = optim_results['optimized_S']
+        DS_opt_d = optim_results['optimized_d']
+    else:
+        S_opt_d, DS_opt_d = optimizer_obj()
+        optim_results = dict(extractor_name=extract_id,
+                             model_spec=extractor_obj.model_spec,
+                             layer_spec=extractor_obj.layer_spec,
+                             data_type=extractor_obj.extract_type,
+                             benchmark=extractor_obj.extract_benchmark,
+                             average=extractor_obj.average_sentence,
+                             optimizatin_name=optimizer_id,
+                             optimized_S=S_opt_d,
+                             optimized_d=DS_opt_d,
+                             bins=bins,
+                             epsilon=epsilon,
+                             kl_div_threshold=optimizer_obj.kl_div_threshold,
+                             kl_div_muliplier=optimizer_obj.kl_div_muliplier,
+                             corr_min_max=list(zip(min_vals, max_vals)),
+                             kl_div_max_mult=kl_multiplier)
+
+             # check of path is too long
+        save_obj(optim_results, optim_file)
 
