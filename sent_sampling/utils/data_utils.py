89030d1d285f878f891bb9dfb482dab308bb2bf0
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2025-02-19 11:32:30 -0500

    adding context sentences

--- sent_sampling/utils/data_utils.py
+++ sent_sampling/utils/data_utils.py
@@ -318,8 +318,8 @@ SENTENCE_CONFIG = [
     dict(name='pereira2018_243sentences',file_loc=os.path.join(COCA_PREPROCESSED_DIR,'')),
     dict(name='pereira2018_384sentences',file_loc=os.path.join(COCA_PREPROCESSED_DIR,'')),
     dict(name='timit_sentences',file_loc=os.path.join(TIMIT_DIR, 'TIMIT_clean_v1.pkl')),
-    dict(name='ANNSet1_with_context',file_loc=os.path.join(TIMIT_DIR, 'ANNSet1_with_context.pkl')),
-    dict(name='ANNSet1_with_random_context', file_loc=os.path.join(TIMIT_DIR, 'ANNSet1_with_random_context.pkl'))]
+    dict(name='ANNSet1_with_context',file_loc=os.path.join(UD_PARENT, 'ANNSet1_with_context.pkl')),
+    dict(name='ANNSet1_with_random_context', file_loc=os.path.join(UD_PARENT, 'ANNSet1_with_random_context.pkl'))]
 
 
 
