282b0372d0a8ae810cb025248a2fc1f772daf622
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2025-02-19 18:19:51 -0500

    adding context sentences

--- bash/run_extract_model_act_for_group.sh
+++ bash/run_extract_model_act_for_group.sh
@@ -6,9 +6,9 @@ GRAND_PIPE_FILE="${DATA_DIR}/Grand_extraction_pipe_list.csv"
 rm -f $GRAND_PIPE_FILE
 touch $GRAND_PIPE_FILE
 #models="roberta-base xlnet-large-cased bert-large-uncased-whole-word-masking xlm-mlm-en-2048 gpt2-xl albert-xxlarge-v2 ctrl"
-#layers="13 25 25 13 44 13 49"
+#layers="13 25 25 13 49 13 49"
 models="gpt2-xl"
-layers="44" # adding the embedding layer so its layer plus 1
+layers="49" # adding the embedding layer so its layer plus 1
 #
 
 model_arr=($models)
